d5af58f2569a05ffc62266dcff2726f3fcf2b466
vanta blanta <vantablanta@gmail.com>
2022-04-24 15:38:31 +0300

    credentials class defiend with delet and search methods

--- user.py
+++ user.py
@@ -1,19 +1,66 @@
 import random
 import string
 
+
 class User:
     """"""
-    user_list = []
+    users = []
 
-    def __init__(self, account, username, password ):
+    def __init__(self, username, password):
         self.username = username
-        self.account = account
         self.password = password
 
     def save_user(self):
-            User.user_list.append(self)
+        User.users.append(self)
 
-    def delete(self):
-        User.user_list.remove(self)
-  
+    def delete_user(self):
+        User.users.remove(self)
 
+    @classmethod
+    def display_users(cls):
+        return cls.users
+
+    @classmethod
+    def find_user__by_number(cls, number):
+        for user in cls.users:
+            if user.password == number:
+                return user
+
+    @classmethod
+    def user_exists(cls, number):
+        for user in cls.users:
+            if user.username == number:
+                return True
+
+
+class Credentials:
+    """"""
+    accounts = []
+
+    def __init__(self, account_username, account_name, account_password):
+        self.account_username = account_username
+        self.account_name = account_name
+        self.account_password = account_password
+
+    def save_account(self):
+        Credentials.accounts.append(self)
+
+    def delete_account(self):
+        Credentials.accounts.remove(self)
+
+    @classmethod
+    def display_account(cls):
+        # for account in cls.accounts:
+           return cls.accounts
+
+    @classmethod
+    def find_account_by_number(cls, number):
+        for account in cls.accounts:
+            if account.password == number:
+                return account
+
+    @classmethod
+    def user_exists(cls, number):
+        for account in cls.accounts:
+            if account.account_username == number:
+                return True
