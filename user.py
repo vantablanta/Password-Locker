bf6e7d8cf5a8f539fde89d5f9c548f1dca476958
vanta blanta <vantablanta@gmail.com>
2022-04-25 07:18:58 +0300

    dc: Documenttaion, DOcstrings added

--- user.py
+++ user.py
@@ -9,23 +9,38 @@ class User:
         self.password = password
 
     def save_user(self):
+        """
+        Function to save a user
+        """
         User.users.append(self)
 
     def delete_user(self):
+        """
+        Function to delete a user 
+        """
         User.users.remove(self)
 
     @classmethod
     def display_users(cls):
+        """
+        Function to save a user
+        """
         return cls.users
 
     @classmethod
     def find_user__by_number(cls, number):
+        """
+        Function to find a user by username
+        """
         for user in cls.users:
             if user.password == number:
                 return user
 
     @classmethod
     def user_exists(cls, number):
+        """
+        Function to check if a user exists
+        """
         for user in cls.users:
             if user.username == number:
                 return True
@@ -43,9 +58,15 @@ class Credentials:
         self.account_password = account_password
 
     def save_account(self):
+        """
+        Function to save an account
+        """
         Credentials.accounts.append(self)
 
     def delete_account(self):
+        """
+        Function to delete and account
+        """
         Credentials.accounts.remove(self)
 
     @classmethod
@@ -66,6 +87,9 @@ class Credentials:
 
     @classmethod
     def user_exists(cls, number):
+        """
+        Method to check if a  user exists
+        """
         for account in cls.accounts:
             if account.account_username == number:
                 return True
