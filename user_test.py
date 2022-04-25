8cc55c81d621688b4a9eb0924ed85249f1ed5e99
vanta blanta <vantablanta@gmail.com>
2022-04-25 05:59:59 +0300

    Credentials object created succesfull

--- user_test.py
+++ user_test.py
@@ -5,13 +5,18 @@ from user import Credentials
 class TestUser(unittest.TestCase):
     """"""
     new_user = User("Michelle", "pass")
+    new_credentials = Credentials("njeri", "facebook", "1234")
 
     def setup(self):
         self.new_user = User("Michelle", "pass")
+        self.new_credentials = Credentials("njeri", "facebook", "1234")
 
     def test_init(self):
         self.assertEqual(self.new_user.username, "Michelle")
         self.assertEqual(self.new_user.password, "pass")
+        self.assertEqual(self.new_credentials.account_username, "njeri")
+        self.assertEqual(self.new_credentials.account_name, "facebook")
+        self.assertEqual(self.new_credentials.account_password, "1234")
 
     def test_save_user(self):
         self.new_user.save_user()
